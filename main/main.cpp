31c028bca3846a6d49529ed1c6bde5dec140e15e
wojtekkkkkkowww <268486@student.pwr.edu.pl>
2024-10-12 16:57:46 +0200

    improve cmake

--- main/main.cpp
+++ main/main.cpp
@@ -1,37 +1,11 @@
-#include <SFML/Graphics.hpp>
 #include "Menu.hpp"
 #include "GameWindow.hpp"
 
 int main()
 {
-    // sf::RenderWindow menuWindow(sf::VideoMode(800, 600), "Main Menu");
     
-
-    // Menu menu(font);
-
-    // while (menuWindow.isOpen())
-    // {
-    //     sf::Event event;
-    //     while (menuWindow.pollEvent(event))
-    //     {
-    //         if (event.type == sf::Event::Closed)
-    //             menuWindow.close();
-
-    //         if (event.type == sf::Event::MouseButtonPressed)
-    //         {
-    //             if (menu.isPvPClicked(sf::Vector2f(event.mouseButton.x, event.mouseButton.y)))
-    //             {
-    // menuWindow.close();
     GameWindow gameWindow;
     gameWindow.run();
-    //             }
-    //         }
-    //     }
-
-    //     menuWindow.clear();
-    //     menu.draw(menuWindow);
-    //     menuWindow.display();
-    // }
-
+    
     return 0;
 }
